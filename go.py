ed794d27da10af4bdb6c4879c6ed249c8fe90708
Olaf Gladis <olaf@pokermania.de>
2014-11-24 13:17:57 +0100

    new nick

--- go.py
+++ go.py
@@ -366,7 +366,7 @@ class HWM(NetworkClient):
         loop.call_later(0.25, self.update_bombs)
 
     def connection_established(self):
-        self.send_msg(dict(type="connect", username="hwm"))
+        self.send_msg(dict(type="connect", username="Theseus by hwm"))
         self.send_msg(dict(type="whoami"))
         self.send_msg(dict(type="map"))
 
